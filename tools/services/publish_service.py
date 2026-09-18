f6a808c4e3190517c3d211f65f2459ac08d61276
TakWolf <takwolf@foxmail.com>
2026-09-18 15:50:32 +0800

    调整变量名

--- tools/services/publish_service.py
+++ tools/services/publish_service.py
@@ -14,13 +14,14 @@ def make_release_zips(font_sizes: Sequence[int], font_formats: Sequence[FontForm
     path_define.RELEASES_DIR.mkdir(parents=True, exist_ok=True)
 
     for font_format in font_formats:
-        file_path = path_define.RELEASES_DIR.joinpath(f'xiaodianzhen-pixel-font-{font_format}-v{configs.VERSION}.zip')
-        with ZipFile(file_path, 'w') as file:
+        zip_file_path = path_define.RELEASES_DIR.joinpath(f'xiaodianzhen-pixel-font-{font_format}-v{configs.VERSION}.zip')
+        with ZipFile(zip_file_path, 'w') as file:
             file.write(path_define.PROJECT_ROOT_DIR.joinpath('LICENSE-FONT.md'), 'README.md')
+
             for font_size in font_sizes:
-                file_path = path_define.OUTPUTS_DIR.joinpath(f'xiaodianzhen-{font_size}px.{font_format}')
-                file.write(file_path, file_path.name)
-        logger.info("Make release zip: '{}'", file_path)
+                font_file_path = path_define.OUTPUTS_DIR.joinpath(f'xiaodianzhen-{font_size}px.{font_format}')
+                file.write(font_file_path, font_file_path.name)
+        logger.info("Make release zip: '{}'", zip_file_path)
 
 
 def update_www(font_sizes: Sequence[int]) -> None:
@@ -31,6 +32,7 @@ def update_www(font_sizes: Sequence[int]) -> None:
     for path_from in path_define.OUTPUTS_DIR.iterdir():
         if not path_from.name.endswith('.otf.woff2'):
             continue
+
         path_to = path_from.copy_into(path_define.WWW_FONTS_DIR)
         logger.info("Copy file: '{}' -> '{}'", path_from, path_to)
 
